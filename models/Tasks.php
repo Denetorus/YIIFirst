fee33a47e171ae6e65e73fed82fa50869c511834
Denetor <gfc-com@mail.ru>
2018-04-13 20:13:28 +0300

    Создана модель TASKS с использование gii->create model

---
+++ models/Tasks.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "tasks".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $date
+ * @property int $user_id
+ * @property string $description
+ *
+ * @property Users $user
+ */
+class Tasks extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'tasks';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'date', 'user_id'], 'required'],
+            [['date'], 'safe'],
+            [['user_id'], 'default', 'value' => null],
+            [['user_id'], 'integer'],
+            [['description'], 'string'],
+            [['name'], 'string', 'max' => 255],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => Users::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'date' => 'Date',
+            'user_id' => 'User ID',
+            'description' => 'Description',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(Users::className(), ['id' => 'user_id']);
+    }
+}
